8c025bcf9e4da44e56aadf8d18c0d90f92917b76
Joshua Lease <klezst@gmail.com>
2021-01-29 18:24:14 -0500

    Update code be pass flake8 linting
    Lint away all the pain of the day!

--- MysteryGang/MysteryGang.py
+++ MysteryGang/MysteryGang.py
@@ -1,34 +1,29 @@
-import arcade
 from datetime import datetime
-import random
+
+import arcade
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "wooooo"
 
-class MyGame(arcade.Window):
-    """
-    Main window for test game.
 
-    weeee!
-    """
+class MyGame(arcade.Window):
+    """Main window for test game."""
 
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.CYAN)
 
     def setup(self):
-        """ Set up the game variables. Call to re-start the game. """
+        """Set up the game variables. Call to re-start the game."""
         # Create your sprites and sprite lists here
         pass
 
     def on_draw(self):
-        """
-        Render the screen.
-        """
+        """Render the screen."""
 
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
+        # This command should happen before we start drawing. It will clear the
+        # screen to the background color, and erase what we drew last frame.
         arcade.start_render()
 
     def on_update(self, delta_time):
@@ -40,8 +35,7 @@ class MyGame(arcade.Window):
         pass
 
     def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
+        """Called whenever a key on the keyboard is pressed.
 
         For a full list of keys, see:
         http://arcade.academy/arcade.key.html
@@ -49,36 +43,29 @@ class MyGame(arcade.Window):
         pass
 
     def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
+        """Called whenever the user lets off a previously pressed key."""
         pass
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
+        """Called whenever the mouse moves."""
         pass
 
     def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
+        """Called when the user presses a mouse button."""
         now = datetime.now()
         print('pressed x:{} y:{} @ {}'.format(x, y, now))
 
     def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when the user unpresses a mouse button.
-        """
+        """Called when the user unpresses a mouse button."""
         pass
 
 
 def main():
-    """ Main method """
+    """Main method"""
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
     arcade.run()
+
 
 if __name__ == "__main__":
     main()
