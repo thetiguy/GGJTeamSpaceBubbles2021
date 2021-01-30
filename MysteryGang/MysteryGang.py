41e768799cf8d24c0df09a277fab774b56d8ab4e
Charlie Crandall <thetiguy@gmail.com>
2021-01-29 19:56:40 -0500

    Add splash screen

--- MysteryGang/MysteryGang.py
+++ MysteryGang/MysteryGang.py
@@ -7,11 +7,14 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = "wooooo"
 
 
-class MyGame(arcade.Window):
+class GameView(arcade.View):
     """Main window for test game."""
 
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
+    def __init__(self):
+        super().__init__()
+
+    def on_show(self):
+        """ This is run once when we switch to this view """
         arcade.set_background_color(arcade.color.CYAN)
 
     def setup(self):
@@ -60,12 +63,57 @@ class MyGame(arcade.Window):
         pass
 
 
+class OpeningView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+
+    def on_show(self):
+        """ This is run once when we switch to this view """
+        arcade.set_background_color(arcade.color.CELESTIAL_BLUE)
+
+        # Reset the viewport, necessary if we have a scrolling game and we need
+        # to reset the viewport back to the start so we can see what we draw.
+        arcade.set_viewport(0, SCREEN_WIDTH - 1, 0, SCREEN_HEIGHT - 1)
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """Called when the user presses a mouse button."""
+        game_view = GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
+
+    def on_draw(self):
+        """ Draw this view """
+        arcade.start_render()
+        arcade.draw_text("Welcome", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+                         arcade.color.WHITE, font_size=50, anchor_x="center")
+        arcade.draw_text("Click to go", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2-75,
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
+
+
+class EndingView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+
+    def on_show(self):
+        """ This is run once when we switch to this view """
+        arcade.set_background_color(arcade.color.BLUE)
+
+        # Reset the viewport, necessary if we have a scrolling game and we need
+        # to reset the viewport back to the start so we can see what we draw.
+        arcade.set_viewport(0, SCREEN_WIDTH - 1, 0, SCREEN_HEIGHT - 1)
+
 def main():
     """Main method"""
-    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game.setup()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+    intro_view = OpeningView()
+    window.show_view(intro_view)
+
     arcade.run()
 
 
 if __name__ == "__main__":
     main()
+
