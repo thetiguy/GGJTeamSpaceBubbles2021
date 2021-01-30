ea8c2a4a7e531619f09a2241dbd5f92c0490351b
Charlie Crandall <thetiguy@gmail.com>
2021-01-30 18:08:21 -0500

    Add basic text box to chatpane

--- MysteryGang/gui/ChatPane.py
+++ MysteryGang/gui/ChatPane.py
@@ -3,31 +3,41 @@ from datetime import datetime
 import arcade
 
 from . import Pane
+from ..constants import FONTS
 
 
 class ChatPane(Pane):
     """A panel that will look and act sort of like hangouts."""
 
-    chat_buffer = []
     messages = []
 
     def __init__(self, left, right, top, bottom, ui_manager):
         """Set up the Chat section of the game screen."""
-        super().__init__(left, right, top, bottom)
+        super().__init__(left, right, top, bottom,
+                         background_color=arcade.color.ANTI_FLASH_WHITE,
+                         border_color=arcade.color.DARK_PASTEL_GREEN)
+        self.ui_manager = ui_manager
+
+        buff = self.border_width / 2
+        send_left = self.left + buff
+        send_right = self.right - buff
+        send_bottom = self.bottom
+        send_top = send_bottom + 40
+        self.send_box = ChatBox(send_left, send_right, send_top, send_bottom)
 
     def send_key(self, key):
         """Handles keys from main UI."""
-        self.chat_buffer.append(key)
+        self.send_box.send_key(key)
 
     def show_msg_buffer(self):
-        msg = ''.join(self.chat_buffer)
+        msg = self.send_box.get_current_msg()
         print(f'[{msg}]')
 
     def send_msg_buffer(self):
-        msg = ''.join(self.chat_buffer)
+        msg = self.send_box.get_current_msg()
         self.show_msg_buffer()
         self.send_msg('Worker', msg)
-        self.chat_buffer = []
+        self.send_box.clear()
 
     def send_msg(self, target, message):
         """Send a message from the chat to a worker."""
@@ -46,9 +56,6 @@ class ChatPane(Pane):
         print('Ding!')
         self.render_messages()
 
-    def render_chat_input(self):
-        pass
-
     def render_messages(self):
         """display messages in the chat window."""
 
@@ -59,23 +66,47 @@ class ChatPane(Pane):
 
     def on_draw(self):
         """Draw the chat box elements."""
-        border_width = 20
-        buff = border_width / 2
-
-        # Draw the border of the pane
-        arcade.draw_lrtb_rectangle_outline(
-            self.left, self.right, self.top, self.bottom, arcade.color.BLACK,
-            border_width)
-
-        arcade.draw_lrtb_rectangle_filled(
-            self.left + buff, self.right - buff,
-            self.top - buff, self.bottom + buff,
-            arcade.color.WHITE)
+        super().on_draw()
+        self.send_box.on_draw()
 
 
 class ChatBox(Pane):
     """User types messages here"""
-    pass
+
+    content = []
+
+    def __init__(self, left, right, top, bottom):
+        """Set up the Chat section of the game screen."""
+        super().__init__(left, right, top, bottom,
+                         background_color=arcade.color.WHITE,
+                         border_color=arcade.color.DARK_GRAY,
+                         border_width=4)
+
+    def send_key(self, key):
+        self.content.append(key)
+
+    def clear(self):
+        self.content = []
+
+    def get_current_msg(self):
+        return ''.join(self.content)
+
+    def on_draw(self):
+        """Draw the chat box elements."""
+        super().on_draw()
+
+        text_color = arcade.color.BLACK
+        text = self.get_current_msg()
+
+        if text == '':
+            text = 'Send a message'
+            text_color = arcade.color.DARK_GRAY
+
+        arcade.draw_text(text,
+                         self.left + self.border_width,
+                         self.bottom + self.border_width,
+                         text_color, font_size=20, font_name=FONTS,
+                         anchor_x="left", anchor_y="bottom")
 
 
 class ChatMessage:
