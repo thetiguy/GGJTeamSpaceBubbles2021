7e93150e1ac5265efad70ee6a8eca7c691026a82
Watrick117 <60557071+Watrick117@users.noreply.github.com>
2021-01-30 22:18:49 -0500

    Update constants.py

--- MysteryGang/constants.py
+++ MysteryGang/constants.py
@@ -2,5 +2,14 @@
 ASSET_PREFIX = 'assets/{0}'
 CLUE_PREFIX = ASSET_PREFIX.format('clues/{0}')
 MUSIC_PREFIX = ASSET_PREFIX.format('music/{0}')
+<<<<<<< Updated upstream
 FONTS = ['Arial', 'Cantarell-Regular']
 BORDER_WIDTH = 20
+=======
+REPORT_FREQUENCY = 10
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+SCREEN_TITLE = "wooooo"
+
+MORTEEVITA = False # Used for testing life and death endings
+>>>>>>> Stashed changes
